1b0858dc9ab952172079cedf2ce0947dd2f9783e
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-07 00:46:55 +0200

    Add Typography.

--- src/templates/DashboardTemplate.tsx
+++ src/templates/DashboardTemplate.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import { Typography } from '@mui/material';
 
 const DashboardTemplate: FC = () => {
-    return <div>dashboardTemplate</div>;
+    return (
+        <div>
+            <Typography variant="h1">Invoice App</Typography>
+        </div>
+    );
 };
 
 export default DashboardTemplate;
