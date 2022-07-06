619671fc187a9e68c62f9bbd18aa1ebd71e84795
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-07 00:45:15 +0200

    Create mainTheme.

---
+++ src/themes/mainTheme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+    interface Theme {
+        typography: {
+            fontSize: number;
+        };
+    }
+    interface TypographyOptions {
+        typography?: {
+            fontSize?: string;
+        };
+    }
+}
+
+export const theme = createTheme({
+    typography: {
+        fontSize: 10,
+    },
+});
