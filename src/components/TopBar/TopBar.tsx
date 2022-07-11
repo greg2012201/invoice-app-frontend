27c515db3b6418548623976999d61e30adae435b
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-11 22:56:06 +0200

    Get handler from props.

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,16 +1,28 @@
 import React, { FC } from 'react';
 import { Typography, AppBar, Toolbar, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { DRAWER_WIDTH } from 'consts/navigation';
 
-const TopBar: FC = () => {
+interface Props {
+    handleDrawerToggle: () => void;
+}
+
+const TopBar: FC<Props> = ({ handleDrawerToggle }) => {
     return (
-        <AppBar position="static">
+        <AppBar
+            position="fixed"
+            sx={{
+                width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
+                ml: { sm: `${DRAWER_WIDTH}px` },
+            }}
+        >
             <Toolbar variant="dense">
                 <IconButton
                     edge="start"
                     color="inherit"
                     aria-label="menu"
                     sx={{ mr: 2 }}
+                    onClick={handleDrawerToggle}
                 >
                     <MenuIcon />
                 </IconButton>
