764ee64a28b1b7b4e3b5d664e47b8e9382836308
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-11 22:42:30 +0200

    Create LeftBar component.

---
+++ src/components/LeftBar/LeftBar.tsx
@@ -0,0 +1,64 @@
+import React, { FC } from 'react';
+import {
+    Toolbar,
+    Divider,
+    Typography,
+    Drawer,
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+} from '@mui/material';
+import { DRAWER_WIDTH, SECTIONS } from 'consts/navigation';
+
+const LeftBar: FC = () => {
+    const container = window.document.body;
+    const drawer = (
+        <div>
+            <Toolbar>
+                <Typography>Invoice App</Typography>
+            </Toolbar>
+            <Divider />
+            <List>
+                {SECTIONS.map((section: string) => (
+                    <ListItem key={section}>
+                        <ListItemButton>{section}</ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+    return (
+        <Box
+            component="nav"
+            sx={{ width: { sm: DRAWER_WIDTH }, flexShrink: { sm: 0 } }}
+            aria-label="sections"
+        >
+            <Drawer
+                container={container}
+                variant="temporary"
+                open
+                ModalProps={{
+                    keepMounted: true,
+                }}
+            >
+                {drawer}
+            </Drawer>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': {
+                        boxSizing: 'border-box',
+                        width: DRAWER_WIDTH,
+                    },
+                }}
+                open
+            >
+                {drawer}
+            </Drawer>
+        </Box>
+    );
+};
+
+export default LeftBar;
