b922822a4fadf3e50b39412f6b6e22ce8613e434
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-13 23:09:25 +0200

    Add TopBar and LeftBar to the Navigation.

--- src/templates/Navigation.tsx
+++ src/templates/Navigation.tsx
@@ -1,7 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import TopBar from 'components/TopBar';
+import LeftBar from 'components/LeftBar';
 
 const Navigation: FC = () => {
-    return <div>Navigation</div>;
+    const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+    const handleDrawerToggle: () => void = () => {
+        return setMobileOpen((prevState: boolean) => !prevState);
+    };
+    return (
+        <>
+            <TopBar handleDrawerToggle={handleDrawerToggle} />
+            <LeftBar mobileOpen={mobileOpen} />
+        </>
+    );
 };
 
 export default Navigation;
