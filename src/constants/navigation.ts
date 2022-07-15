3c49193172690b69982a55601d8f5e800560ae81
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-16 00:01:50 +0200

    Add paths to the navigation section data.

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,8 +1,13 @@
-export const SECTIONS: Array<string> = [
-    'Issue Invoice',
-    'Incoming Invoices',
-    'Issued Invoices List',
-    'Settings',
+interface Sections {
+    name: string;
+    path: string;
+}
+
+export const SECTIONS: Array<Sections> = [
+    { name: 'Issue Invoice', path: '/issue-invoice' },
+    { name: 'Incoming Invoice', path: '/incoming-invoice' },
+    { name: 'Issued Invoices List', path: '/issue-invoice-list' },
+    { name: 'Settings', path: '/settings' },
 ];
 
 export const DRAWER_WIDTH = 240;
