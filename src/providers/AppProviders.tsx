85fc620bffe2eb4adc046a3bd30cfa3b7d048c9f
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-16 00:30:41 +0200

    Cleanup

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from 'themes/mainTheme';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 interface Props {
     children: JSX.Element;
