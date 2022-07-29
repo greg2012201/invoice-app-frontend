a52a16df177a28e38ff0ef8f54356923b9f12257
Grzegorz Dubiel <greg2012201@gmail.com>
2022-07-29 23:48:35 +0200

    Do not use hook in toggle condition.

--- src/templates/Navigation.tsx
+++ src/templates/Navigation.tsx
@@ -1,13 +1,11 @@
 import React, { FC, useState } from 'react';
 import TopBar from 'components/TopBar';
 import LeftBar from 'components/LeftBar';
-import { useWindowSize } from 'hooks/useWindowSize';
 
 const Navigation: FC = () => {
     const [mobileOpen, setMobileOpen] = useState<boolean>(false);
-    const { width } = useWindowSize();
     const handleDrawerToggle: () => void = () => {
-        if (width >= 600) {
+        if (window.innerWidth >= 600) {
             return;
         }
         setMobileOpen((prevState: boolean) => !prevState);
