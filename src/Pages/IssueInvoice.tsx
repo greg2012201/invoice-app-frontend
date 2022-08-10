c9062540decf6e148556cddf0034dba7181470d0
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-11 00:32:59 +0200

    Add heading to the IssueInvoice page.

--- src/Pages/IssueInvoice.tsx
+++ src/Pages/IssueInvoice.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from 'react';
-import { Typography, Divider } from '@mui/material';
+import { Typography, Divider, Box } from '@mui/material';
 import IssueInvoiceForm from 'components/IssueInvoiceForm';
 
 const IssueInvoice: FC = () => {
     return (
         <div>
-            <Typography variant="h2">Issue invoice</Typography>
-            <Typography variant="subtitle2">
-                Provide information to issue new invoice
-            </Typography>
+            <Box>
+                <Typography variant="h2">Issue invoice</Typography>
+                <Typography variant="subtitle2">
+                    Provide information to issue new invoice
+                </Typography>
+            </Box>
             <Divider />
             <IssueInvoiceForm />
         </div>
