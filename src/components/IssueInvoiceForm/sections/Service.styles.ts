2bc8c25b50ff5732ab9e3cee40435c21a43994dc
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-13 01:06:04 +0200

    Add more space for form.

--- src/components/IssueInvoiceForm/sections/Service.styles.ts
+++ src/components/IssueInvoiceForm/sections/Service.styles.ts
@@ -4,4 +4,4 @@ type TextFieldStyle = {
 export const TextFieldStyle = {
     width: '100%',
 };
-export const Wrapper = { flexGrow: 1, flexWrap: 'wrap', maxWidth: '900px' };
+export const Wrapper = { flexGrow: 1, flexWrap: 'wrap', maxWidth: '1300px' };
