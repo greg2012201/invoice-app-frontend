f414f859c389a06d1c0196061091c02a99b7b8c6
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-13 01:03:42 +0200

    Make TopBar fit to Drawer toolbar.

--- src/components/TopBar/TobBar.styles.ts
+++ src/components/TopBar/TobBar.styles.ts
@@ -1,7 +1,10 @@
 import { DRAWER_WIDTH } from 'constants/navigation';
 
 export const Wrapper = {
+    display: 'flex',
+    justifyContent: 'center',
     width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
     ml: { sm: `${DRAWER_WIDTH}px` },
+    height: '64px',
 };
 export const Icon = { mr: 2, display: { sm: 'none' } };
