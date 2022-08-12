74006ac015256453639b77d9a2ac3b8c89d66d22
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-13 01:06:36 +0200

    Add more form fields & update form layout.

--- src/components/IssueInvoiceForm/sections/Service.tsx
+++ src/components/IssueInvoiceForm/sections/Service.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
-import { Box, TextField, Grid, Input } from '@mui/material';
+import { Box, TextField, Grid } from '@mui/material';
 import { TextFieldStyle, Wrapper } from './Service.styles';
 
 const Service: FC = () => {
     return (
         <Box component="form" sx={Wrapper}>
             <Grid container spacing={2}>
-                <Grid item xs={8} md={8} lg={5}>
+                <Grid item xs={8} md={8} lg={3.5}>
                     <TextField
                         style={TextFieldStyle}
                         fullWidth
@@ -27,7 +27,7 @@ const Service: FC = () => {
                         variant="outlined"
                     />
                 </Grid>
-                <Grid item xs={8} md={5} lg={2.5}>
+                <Grid item xs={8} md={5} lg={1.5}>
                     <TextField
                         style={TextFieldStyle}
                         fullWidth
@@ -38,15 +38,59 @@ const Service: FC = () => {
                         variant="outlined"
                     />
                 </Grid>
-                <Grid item xs={8} md={5} lg={2.5}>
+                <Grid item xs={8} md={5} lg={1.5}>
                     <TextField
                         style={TextFieldStyle}
                         fullWidth
                         type="number"
-                        id="price-net"
-                        name="price-gross"
-                        label="Price gross"
+                        id="value-net"
+                        name="value-net"
+                        label="Value net"
                         variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={8} md={5} lg={1.2}>
+                    <TextField
+                        style={TextFieldStyle}
+                        fullWidth
+                        type="number"
+                        id="vat-rate"
+                        name="vat-rate"
+                        label="VAT rate"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={8} md={5} lg={1.5}>
+                    <TextField
+                        style={TextFieldStyle}
+                        fullWidth
+                        type="number"
+                        id="sum-vat"
+                        name="sum-vat"
+                        label="Sum VAT"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={8} md={5} lg={1.5}>
+                    <TextField
+                        style={TextFieldStyle}
+                        fullWidth
+                        type="number"
+                        id="gross-value"
+                        name="gross-value"
+                        label="Gross value"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={8} md={8} lg={3.5}>
+                    <TextField
+                        id="comments"
+                        name="comments"
+                        label="Comments"
+                        style={TextFieldStyle}
+                        multiline
+                        maxRows={4}
+                        minRows={4}
                     />
                 </Grid>
             </Grid>
