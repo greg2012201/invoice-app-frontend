1dbdd62c671dab223a338ca413cd1ff780c031a1
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-15 11:53:24 +0200

    Mam contractors in Contractors component.

--- src/components/IssueInvoiceForm/sections/Contractors/Contractors.tsx
+++ src/components/IssueInvoiceForm/sections/Contractors/Contractors.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react';
 import { Box } from '@mui/material';
+import { contractors } from 'mocks/contractors';
+import { Contractor } from 'types/contractors';
 import { Wrapper } from './Contractors.styles';
 import ContractorCard from './ContractorCard';
 
 const Contractors: FC = () => {
     return (
         <Box sx={Wrapper}>
-            <ContractorCard />
+            {Object.entries(contractors).map(
+                ([key, value]: [key: string, value: Contractor]) => {
+                    const contractor = value;
+                    return (
+                        /* eslint-disable-next-line */
+                        <ContractorCard contractorType={key} {...contractor} />
+                    );
+                }
+            )}
         </Box>
     );
 };
