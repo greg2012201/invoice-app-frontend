a949f44cf801feed3a6c3456a8d98ae6cfce83df
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-18 00:31:03 +0200

    Add data flow logic & structure.

---
+++ src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.styles.ts
@@ -0,0 +1,6 @@
+export const ContractorTypeParagraph = { textTransform: 'capitalize' };
+export const ListItemStyles = {
+    gap: '10px',
+    display: 'grid',
+    gridTemplateColumns: '50% 50%',
+};
--- src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.tsx
+++ src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.tsx
@@ -1,13 +1,66 @@
 import React, { FC } from 'react';
-import { Card, Typography, CardContent, Button } from '@mui/material';
+import {
+    Card,
+    Typography,
+    CardContent,
+    Button,
+    CardActions,
+    List,
+    ListItem,
+} from '@mui/material';
+import { Contractor } from 'types/contractors';
+import {
+    ContractorTypeParagraph,
+    ListItemStyles,
+} from './ContractorCard.styles';
 
-const ContractorCard: FC = () => {
+interface Props extends Contractor {
+    contractorType: string;
+}
+type AddressItemLabel = {
+    label: string;
+    value: string;
+};
+const addressItemLabels: AddressItemLabel[] = [
+    { label: 'City', value: 'city' },
+    { label: 'Country', value: 'country' },
+    { label: 'Street', value: 'street' },
+    { label: 'Postal Code', value: 'postalCode' },
+    { label: 'Number', value: 'number' },
+];
+const ContractorCard: FC<Props> = ({ contractorType, name, address }) => {
     return (
-        <Card sx={{ maxWidth: 300 }}>
+        <Card variant="outlined" sx={{ maxWidth: 360 }}>
             <CardContent>
-                <Typography>Contractor name</Typography>
-                <Button>edit</Button>
+                <Typography color="text.secondary" sx={ContractorTypeParagraph}>
+                    {contractorType}
+                </Typography>
+                <Typography variant="h5" component="p">
+                    {name}
+                </Typography>
+                <List>
+                    {Object.entries(address).map(
+                        ([key, value]: [key: string, value: string]) => {
+                            const foundLabel = addressItemLabels.find(
+                                (item: AddressItemLabel) => {
+                                    return key === item.value;
+                                }
+                            )?.label;
+                            return (
+                                <ListItem sx={ListItemStyles}>
+                                    <Typography color="text.secondary">
+                                        {foundLabel}:
+                                    </Typography>
+                                    <Typography>{value}</Typography>
+                                </ListItem>
+                            );
+                        }
+                    )}
+                </List>
             </CardContent>
+            <CardActions>
+                <Button type="button">edit</Button>
+            </CardActions>
         </Card>
     );
 };
