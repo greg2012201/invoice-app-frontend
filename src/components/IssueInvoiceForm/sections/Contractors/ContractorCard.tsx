05ef47e464c3710bb97c4ba5796a9996f0e76663
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-22 16:37:53 +0200

    Add optional items, and add sharedProps for item label.

--- src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.tsx
+++ src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.tsx
@@ -18,12 +18,21 @@ import {
 interface Props extends Contractor {
     contractorType: string;
 }
-
-const ContractorCard: FC<Props> = ({ contractorType, name, address }) => {
+const sharedItemLabelProps = { color: 'text.secondary', fontStyle: 'italic' };
+const ContractorCard: FC<Props> = ({
+    contractorType,
+    name,
+    address,
+    tel,
+    email,
+}) => {
     return (
         <Card variant="outlined" sx={CardWrapperStyles}>
             <CardContent>
-                <Typography color="text.secondary" sx={ContractorTypeParagraph}>
+                <Typography
+                    sx={ContractorTypeParagraph}
+                    {...sharedItemLabelProps}
+                >
                     {contractorType}
                 </Typography>
                 <Typography variant="h5" component="p">
@@ -31,13 +40,31 @@ const ContractorCard: FC<Props> = ({ contractorType, name, address }) => {
                 </Typography>
                 <List>
                     <ListItem sx={ListItemStyles}>
-                        <Typography color="text.secondary">Address:</Typography>
+                        <Typography {...sharedItemLabelProps}>
+                            Address:
+                        </Typography>
                         <Typography>
                             {address.street} {address.number}, <br />
                             {address.postalCode} {address.city},{' '}
                             {address.country}
                         </Typography>
                     </ListItem>
+                    {tel && (
+                        <ListItem sx={ListItemStyles}>
+                            <Typography {...sharedItemLabelProps}>
+                                Tel:
+                            </Typography>
+                            <Typography>{tel}</Typography>
+                        </ListItem>
+                    )}
+                    {email && (
+                        <ListItem sx={ListItemStyles}>
+                            <Typography {...sharedItemLabelProps}>
+                                Email:
+                            </Typography>
+                            <Typography>{email}</Typography>
+                        </ListItem>
+                    )}
                 </List>
             </CardContent>
             <CardActions>
