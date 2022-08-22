89c6f3a707092379bdffc6d5749376e369d496ae
Grzegorz Dubiel <greg2012201@gmail.com>
2022-08-22 16:01:03 +0200

    Make card fit to the wrapper on small screens.

--- src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.styles.ts
+++ src/components/IssueInvoiceForm/sections/Contractors/ContractorCard.styles.ts
@@ -1,4 +1,7 @@
-export const CardWrapperStyles = {};
+export const CardWrapperStyles = {
+    width: '100%',
+    '@media (min-width:440px)': { width: 350 },
+};
 export const ContractorTypeParagraph = { textTransform: 'capitalize' };
 export const ListItemStyles = {
     gap: '10px',
