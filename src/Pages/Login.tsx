e78afbd0a4481cdde7c251cfcc36251d6b8de290
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-07 22:39:53 +0200

    Add a center wrapper.

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,17 @@
+import { Box } from '@mui/material';
 import React, { FC } from 'react';
 
 const Login: FC = () => {
-    return <div>Login</div>;
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+        >
+            Login
+        </Box>
+    );
 };
 
 export default Login;
