27cabf6bdf6f5432b8ec6a7c9086505ee12817aa
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-12 23:32:20 +0200

    Add config for ApolloClient.

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,16 +2,25 @@ import React, { FC } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from 'themes/mainTheme';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 interface Props {
     children: JSX.Element;
 }
 
+const uri: string | undefined = process.env.API_URI;
+
 const AppProviders: FC<Props> = ({ children }) => {
+    const client = new ApolloClient({
+        uri,
+        cache: new InMemoryCache(),
+    });
     return (
-        <BrowserRouter>
-            <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            </BrowserRouter>
+        </ApolloProvider>
     );
 };
 export default AppProviders;
