3cf14490b68c2cec71e5730b052df3b1ec3fb260
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-12 23:37:03 +0200

    Add an util to set the access token in memory.

---
+++ src/utils/accessToken.ts
@@ -0,0 +1,8 @@
+let accessToken = '';
+
+export const setAccessToken = (token: string): void => {
+    accessToken = token;
+};
+export const getAccessToken = (): string => {
+    return accessToken;
+};
