99d4c09a78dd49e55e495feb8d2e7e248b3e001e
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-19 22:15:40 +0200

    Add a query for login.

---
+++ src/queries/login.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+    mutation Login($email: String, $password: String) {
+        login(email: $email, password: $password) {
+            id
+            email
+            accessToken
+        }
+    }
+`;
