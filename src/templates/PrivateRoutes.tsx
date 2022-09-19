a109add30610f6b3f37c2f55c99f043677028bf2
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-19 22:17:53 +0200

    Get logged in user data.

--- src/templates/PrivateRoutes.tsx
+++ src/templates/PrivateRoutes.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from 'react';
 import { Navigate } from 'react-router';
+import { useQuery } from '@apollo/client';
+import { GET_ME } from 'queries/getMe';
 
 interface Props {
     children: JSX.Element;
 }
 
-const user = { id: null };
-
 const PrivateRoutes: FC<Props> = ({ children }) => {
-    const isUser = user?.id;
-
+    const { data, loading } = useQuery(GET_ME);
+    const isUser = data?.getMe?.id;
+    if (loading) {
+        return <p>loading...</p>;
+    }
     if (!isUser) {
         return <Navigate replace to="/login" />;
     }
