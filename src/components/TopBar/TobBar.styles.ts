17dec5dd12990d46a40282aca0f69cccdb68e6d9
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-21 23:37:29 +0200

    Adjust styles for the Account in TobBar.

--- src/components/TopBar/TobBar.styles.ts
+++ src/components/TopBar/TobBar.styles.ts
@@ -2,7 +2,9 @@ import { DRAWER_WIDTH } from 'constants/navigation';
 
 export const Wrapper = {
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` },
     ml: { sm: `${DRAWER_WIDTH}px` },
     height: '64px',
