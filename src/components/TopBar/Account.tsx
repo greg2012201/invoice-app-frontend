cfcec83d8de7c788111584343e39335e60b75ef1
Grzegorz Dubiel <greg2012201@gmail.com>
2022-09-24 00:35:01 +0200

    Handle logout in the Account component.

--- src/components/TopBar/Account.tsx
+++ src/components/TopBar/Account.tsx
@@ -2,18 +2,22 @@ import React, { FC, useState } from 'react';
 import { IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { GET_ME } from 'queries/getMe';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import { LOGOUT } from 'queries/logout';
 
 const Account: FC = () => {
     const { data, loading } = useQuery(GET_ME);
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [logout] = useMutation(LOGOUT);
     const handleOpenMenu = (e: React.MouseEvent<HTMLElement>): void => {
         setAnchorEl(e?.currentTarget);
     };
     const handleCloseMenu = (): void => {
         setAnchorEl(null);
     };
-    const logout = (): void => {
+    const handleLogout = (): void => {
+        logout();
+        window.location.replace('/');
         handleCloseMenu();
     };
     if (loading) {
@@ -47,7 +51,7 @@ const Account: FC = () => {
                 open={Boolean(anchorEl)}
                 onClose={handleCloseMenu}
             >
-                <MenuItem onClick={logout}>Logout</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
             </Menu>
         </Toolbar>
     );
