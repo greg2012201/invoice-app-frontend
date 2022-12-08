2cb28af325462f87c36bff11cef7360e630360b0
Grzegorz Dubiel <greg2012201@gmail.com>
2022-12-09 00:54:40 +0100

    Update file structure.

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}): JSX.Element {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
