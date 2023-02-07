da125f93410059978261f41003b7b9c3c5dffa22
Grzegorz Dubiel <greg2012201@gmail.com>
2023-02-08 00:52:14 +0100

    Remove react-router-dom from AppPRoviders.

--- providers/AppProviders.tsx
+++ providers/AppProviders.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from 'themes/mainTheme';
-import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 
 import { apolloClientConfig } from 'utils/apolloClientConfig';
@@ -13,9 +12,7 @@ const client = apolloClientConfig;
 const AppProviders: FC<Props> = ({ children }) => {
     return (
         <ApolloProvider client={client}>
-            <BrowserRouter>
-                <ThemeProvider theme={theme}>{children}</ThemeProvider>
-            </BrowserRouter>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </ApolloProvider>
     );
 };
