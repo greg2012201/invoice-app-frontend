63ef3227f36166df512f3aecc3f27c84fd344aba
Grzegorz Dubiel <greg2012201@gmail.com>
2023-02-08 00:44:45 +0100

    Wrapp app with providers.

--- Pages/_app.tsx
+++ Pages/_app.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import CssBaseline from '@mui/material/CssBaseline';
+
 // import Layout from '../components/layout';
+import AppProviders from 'providers/AppProviders';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     // Dashboard Layout should be present here in the future
-    return <Component {...pageProps} />;
+    return (
+        <AppProviders>
+            <CssBaseline>
+                <Component {...pageProps} />
+            </CssBaseline>
+        </AppProviders>
+    );
 };
 
 export default MyApp;
