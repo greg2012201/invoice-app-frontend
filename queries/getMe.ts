0a01b7a0640981078957b08b13376c61dd3212ea
Grzegorz Dubiel <greg2012201@gmail.com>
2023-02-23 00:23:18 +0100

    Divide query.

--- queries/getMe.ts
+++ queries/getMe.ts
@@ -1,11 +1,15 @@
 import { gql } from '@apollo/client';
 
-export const GET_ME = gql`
-    query GetMe {
-        getMe {
-            id
-            name
-            email
-        }
+export const GET_ME_STRING = `
+query GetMe {
+    getMe {
+        id
+        name
+        email
     }
+}
+`;
+
+export const GET_ME = gql`
+    ${GET_ME_STRING}
 `;
