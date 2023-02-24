c10efb13874734c143f1f8e8822539b54033569d
Grzegorz Dubiel <greg2012201@gmail.com>
2023-02-24 23:00:53 +0100

    Imporve typing.

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,22 @@ import { RequestCookies } from 'next/dist/server/web/spec-extension/cookies';
 import { NextRequest, NextResponse } from 'next/server';
 import { GET_ME_STRING } from 'queries/getMe';
 
+interface IAuthResponse {
+    ok: boolean;
+    accessToken: string;
+}
+
+interface IMeResponse {
+    id: string;
+    email: string;
+    name: string;
+}
+
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
-const authFetch = async (
-    cookies: RequestCookies
-): Promise<{ accessToken: string | undefined }> => {
+const authFetch = async (cookies: RequestCookies): Promise<IAuthResponse> => {
     const jid = cookies.get('jid');
 
     return fetch(`${process.env.REACT_APP_REFRESH_TOKEN_URL}`, {
@@ -17,7 +26,7 @@ const authFetch = async (
         headers: { Cookie: `${jid?.name}=${jid?.value}` },
     }).then(res => res.json());
 };
-const meFetch = async (cookies: RequestCookies): Promise<{ id?: string }> => {
+const meFetch = async (cookies: RequestCookies): Promise<IMeResponse> => {
     const accessToken = cookies.get('access_token')?.value;
     return fetch(`${process.env.REACT_APP_API_URI}`, {
         method: 'POST',
@@ -38,14 +47,12 @@ export const middleware = async (
 ): Promise<NextResponse | unknown> => {
     const { cookies } = req;
     const { pathname } = req.nextUrl;
-    const authRes: { ok?: boolean; accessToken?: string } = await authFetch(
-        cookies
-    );
+    const authRes = await authFetch(cookies);
     const response = NextResponse.next();
     if (authRes?.accessToken) {
         response.cookies.set('access_token', authRes.accessToken);
     }
-    const meRes: { id?: string } = await meFetch(cookies);
+    const meRes = await meFetch(cookies);
     if (!meRes?.id && pathname !== '/login' && !authRes?.ok) {
         const loginUrl = new URL('/login', req.url);
         return NextResponse.redirect(loginUrl);
