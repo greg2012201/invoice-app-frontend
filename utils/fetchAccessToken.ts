c1c68649021caca0fbfd1e15f86ce31e7a2ffc3f
Grzegorz Dubiel <greg2012201@gmail.com>
2023-04-05 01:17:45 +0200

    Make fetchAccessToken fn send requests with refresh token.

--- utils/fetchAccessToken.ts
+++ utils/fetchAccessToken.ts
@@ -1,13 +1,27 @@
 const isSSR = typeof window === 'undefined';
 
-export const fetchAccessToken = (): Promise<Response> => {
-    // 'need to inject refresh token too to enable serverside'
-    const refreshTokenURL = isSSR
+const getRefreshTokenUrl = (): string | undefined => {
+    return isSSR
         ? process.env.REFRESH_TOKEN_URL
         : process.env.NEXT_PUBLIC_TOKEN_URL;
-    // fix envs
+};
+
+const isRequestRecord = (value: any): value is Record<string, string> => {
+    return typeof value === 'string' && value.includes('=');
+};
+export const fetchAccessToken = (
+    refreshToken: Record<string, string> | undefined
+): Promise<Response> => {
+    const refreshTokenURL = getRefreshTokenUrl();
+    if (typeof refreshTokenURL !== 'string') {
+        throw new TypeError('Invalid refreshToken type. String is expected.');
+    }
+    const headers: HeadersInit = isRequestRecord(refreshToken)
+        ? refreshToken
+        : {};
     return fetch(`${refreshTokenURL}`, {
         method: 'POST',
         credentials: 'include',
+        headers,
     });
 };
