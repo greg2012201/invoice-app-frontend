9248331875c24a5c02bb7f1eed685b14b861b08e
Grzegorz Dubiel <greg2012201@gmail.com>
2023-04-05 01:21:28 +0200

    Simplify fetcher structure.

--- utils/fetchAccessToken.ts
+++ utils/fetchAccessToken.ts
@@ -6,22 +6,16 @@ const getRefreshTokenUrl = (): string | undefined => {
         : process.env.NEXT_PUBLIC_TOKEN_URL;
 };
 
-const isRequestRecord = (value: any): value is Record<string, string> => {
-    return typeof value === 'string' && value.includes('=');
-};
 export const fetchAccessToken = (
-    refreshToken: Record<string, string> | undefined
+    refreshToken: string | undefined
 ): Promise<Response> => {
     const refreshTokenURL = getRefreshTokenUrl();
     if (typeof refreshTokenURL !== 'string') {
         throw new TypeError('Invalid refreshToken type. String is expected.');
     }
-    const headers: HeadersInit = isRequestRecord(refreshToken)
-        ? refreshToken
-        : {};
     return fetch(`${refreshTokenURL}`, {
         method: 'POST',
         credentials: 'include',
-        headers,
+        headers: { Cookie: `jid=${refreshToken}` },
     });
 };
