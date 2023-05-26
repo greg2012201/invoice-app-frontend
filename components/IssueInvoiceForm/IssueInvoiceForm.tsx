23b833daee9eb3cbebb1d052e76cb80bbdca68db
Grzegorz Dubiel <greg2012201@gmail.com>
2023-05-26 02:07:00 +0200

    Setup react hook form.

--- components/IssueInvoiceForm/IssueInvoiceForm.tsx
+++ components/IssueInvoiceForm/IssueInvoiceForm.tsx
@@ -1,23 +1,35 @@
 import React, { FC } from 'react';
+import { useForm, FormProvider, useFormContext } from 'react-hook-form';
 import { Box, Typography, Button, Toolbar } from '@mui/material';
 import { InvoiceNumber, Form, ToolbarStyles } from './IssueInvoiceForm.styles';
 import Contractors from './sections/Contractors/Contractors';
 import Service from './sections/Service';
 
 const IssueInvoiceForm: FC = () => {
+    const methods = useForm();
+    /* TODO: define types for data */
+    const onSubmit = data => console.log(data);
     return (
-        <Box component="form" sx={Form} noValidate autoComplete="off">
-            <Typography sx={InvoiceNumber}>
-                Invoice Number: FV/01/01/2022
-            </Typography>
-            <Contractors />
-            <Service />
-            <Toolbar disableGutters sx={ToolbarStyles}>
-                <Button type="submit" variant="contained" size="large">
-                    Submit
-                </Button>
-            </Toolbar>
-        </Box>
+        <FormProvider {...methods}>
+            <Box
+                component="form"
+                onSubmit={methods.handleSubmit(onSubmit)}
+                sx={Form}
+                noValidate
+                autoComplete="off"
+            >
+                <Typography sx={InvoiceNumber}>
+                    Invoice Number: FV/01/01/2022
+                </Typography>
+                <Contractors />
+                <Service />
+                <Toolbar disableGutters sx={ToolbarStyles}>
+                    <Button type="submit" variant="contained" size="large">
+                        Submit
+                    </Button>
+                </Toolbar>
+            </Box>
+        </FormProvider>
     );
 };
 
