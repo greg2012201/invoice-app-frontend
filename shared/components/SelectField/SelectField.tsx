6e1cc44890fd0b21f656755c8063f5813ed87b19
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-20 00:40:22 +0200

    Wrapp SelectField component with foward ref.

--- shared/components/SelectField/SelectField.tsx
+++ shared/components/SelectField/SelectField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
@@ -6,11 +6,11 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import type { ReadOnlyMap } from '@/shared/types/shared';
 
-type Option = ReadOnlyMap<{
+type Option = {
     id: string;
     value: string | number;
     label: string;
-}>;
+};
 
 type Props = {
     id: string;
@@ -20,43 +20,47 @@ type Props = {
     helperText?: string;
 };
 
-function SelectField({
-    id,
-    label,
-    options,
-    error = false,
-    helperText,
-    ...rest
-}: ReadOnlyMap<Props>): JSX.Element {
-    const [selectedValue, setSelectedValue] = React.useState('');
+const SelectField = forwardRef<HTMLDivElement, ReadOnlyMap<Props>>(
+    (
+        { id, label, options, error = false, helperText, ...rest }: Props,
+        ref
+    ): JSX.Element => {
+        const [selectedValue, setSelectedValue] = React.useState('');
 
-    const handleChange = (event: SelectChangeEvent): void => {
-        setSelectedValue(event.target.value);
-    };
-    return (
-        <FormControl error={error} sx={{ m: 1, minWidth: 120 }} {...rest}>
-            <InputLabel id={id} error={error}>
-                {label}
-            </InputLabel>
-            <Select
-                labelId={`${id}-label`}
-                id={id}
-                value={selectedValue}
-                label={label}
-                onChange={handleChange}
+        const handleChange = (event: SelectChangeEvent): void => {
+            setSelectedValue(event.target.value);
+        };
+
+        return (
+            <FormControl
                 error={error}
+                sx={{ m: 1, minWidth: 120 }}
+                ref={ref}
+                {...rest}
             >
-                {options.map(option => {
-                    return (
-                        <MenuItem value={option.value}>{option.label}</MenuItem>
-                    );
-                })}
-            </Select>
-            {error && helperText && (
-                <FormHelperText error={error}>{helperText}</FormHelperText>
-            )}
-        </FormControl>
-    );
-}
+                <InputLabel id={id} error={error}>
+                    {label}
+                </InputLabel>
+                <Select
+                    labelId={`${id}-label`}
+                    id={id}
+                    value={selectedValue}
+                    label={label}
+                    onChange={handleChange}
+                    error={error}
+                >
+                    {options.map(option => (
+                        <MenuItem key={option.id} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </Select>
+                {error && helperText && (
+                    <FormHelperText error={error}>{helperText}</FormHelperText>
+                )}
+            </FormControl>
+        );
+    }
+);
 
 export default SelectField;
