9646b5d290ba2c36bc73e2e7eace76bfc18b974a
Grzegorz Dubiel <greg2012201@gmail.com>
2023-07-26 00:07:11 +0200

    Add type util.

--- shared/types/shared.ts
+++ shared/types/shared.ts
@@ -1,5 +1,8 @@
 export type TToken = string | null | undefined;
-
+/* eslint-disable-next-line */
+export type Resolve<T> = T extends Function
+    ? T
+    : { [Property in keyof T]: T[Property] };
 export type ReadOnlyMap<T> = {
     readonly [P in keyof T]: T[P];
 };
