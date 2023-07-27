f5bee86f44572268b3684b3ac033c4b1e45146b7
Grzegorz Dubiel <greg2012201@gmail.com>
2023-07-28 00:05:19 +0200

    Add type util to make partial require type.

--- shared/types/shared.ts
+++ shared/types/shared.ts
@@ -3,7 +3,13 @@ export type TToken = string | null | undefined;
 export type Resolve<T> = T extends Function
     ? T
     : { [Property in keyof T]: T[Property] };
+
+export type PartialRequire<TObj, TRequired extends keyof TObj> = Resolve<
+    Required<Pick<TObj, TRequired>> & Partial<TObj>
+>;
+
 export type ReadOnlyMap<T> = {
     readonly [P in keyof T]: T[P];
 };
+
 export const isString = (x: unknown): x is string => typeof x === 'string';
